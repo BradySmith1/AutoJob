2fb28c2dcaeb86317f5b6f840d14f93a4d987cbc
Brady Smith <bsmith8@catamount.wcu.edu>
2023-09-19 14:32:20 -0400

    Added connection to specific databases and collections. Refactored code to be more clean. (1 hour)

--- backend/src/db_connection.rs
+++ backend/src/db_connection.rs
@@ -1,44 +1,37 @@
 use std::process::exit;
-use mongodb::{Client, options::ClientOptions};
+use mongodb::{Client, Collection, Database, options::ClientOptions};
+use mongodb::bson::{doc, Document};
 
 pub struct DbConnection {
-    client: Option<Client>,
+    client: Client,
+    database: Option<Database>,
+    collection: Option<Collection<Document>>,
 }
 
 impl DbConnection {
 
-    pub fn new()
+    pub async fn new(database_address: String)
                      -> Self {
         DbConnection {
-            client: None,
+            client: connect_mongodb(database_address).await,
+            database: None,
+            collection: None,
         }
     }
 
-    pub async fn connect_database(&mut self, database_address: String) {
-        let client_options = ClientOptions::parse(database_address).await;
-        let mut client_options = match client_options {
-            Ok(client_options) => client_options,
-            Err(_) => {
-                println!("Error: Could not parse address. Check the address rust is trying to connect to.");
-                exit(1);
-            }
-        };
+    pub fn set_database(&mut self, database_name: &str) {
+        let database: Database = self.client.database(database_name);
+        self.database = Option::from(database);
+    }
 
-        client_options.app_name = Some("Job Estimator and Scheduler".to_string());
-
-        let client = Client::with_options(client_options);
-        let client = match client {
-            Ok(client) => client,
-            Err(_) => {
-                println!("Error: Could not connect to the database. Check if mongoDB is running.");
-                exit(1);
-            }
-        };
-        self.client = Option::from(client);
+    pub fn set_collection(&mut self, collection_name: &str){
+        let collection: Collection<Document> = self.database.as_ref().expect("Database hasnt\
+         been initialized").collection(collection_name);
+        self.collection = Option::from(collection);
     }
 
     pub async fn print_databases(&self) {
-        let database_names = self.client.as_ref().expect("Client was never initialized.").list_database_names(None, None).await;
+        let database_names = self.client.list_database_names(None, None).await;
         let database_names = match database_names {
             Ok(database_names) => database_names,
             Err(_) => {
@@ -51,7 +44,30 @@ impl DbConnection {
         }
     }
 
-    pub fn get_client(&self) -> &Option<Client> {
+    pub fn get_client(&self) -> &Client {
         &self.client
     }
+}
+
+async fn connect_mongodb(database_address: String) -> Client {
+    let client_options = ClientOptions::parse(database_address).await;
+    let mut client_options = match client_options {
+        Ok(client_options) => client_options,
+        Err(_) => {
+            println!("Error: Could not parse address. Check the address rust is trying to connect to.");
+            exit(1);
+        }
+    };
+
+    client_options.app_name = Some("Job Estimator and Scheduler".to_string());
+
+    let client = Client::with_options(client_options);
+    let client = match client {
+        Ok(client) => client,
+        Err(_) => {
+            println!("Error: Could not connect to the database. Check if mongoDB is running.");
+            exit(1);
+        }
+    };
+    return client;
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,8 @@ mod db_connection;
 
 #[tokio::main]
 async fn main() {
-    let mut db: DbConnection = DbConnection::new();
-    db.connect_database("mongodb://localhost:27017".to_string()).await;
-    db.print_databases().await;
+    let mut db_conn: DbConnection = DbConnection::new("mongodb://localhost:27017".to_string()).await;
+    db_conn.set_database("ajseDB");
+    db_conn.set_collection("estimates");
+    db_conn.print_databases().await;
 }
