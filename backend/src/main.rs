d7aadbfbeb80b713d9b8c090b8f7d5635bbc536e
Brady Smith <bsmith8@catamount.wcu.edu>
2023-09-20 17:55:10 -0400

    Added the ability to host the web server from the backend. Figuring out how the backend and frontend works. (3 hours)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,52 @@
+extern crate actix_web;
+extern crate actix_files;
+extern crate console;
+
+use actix_files as fs;
+use actix_web::{App, HttpServer, web, middleware, Result, Responder, HttpResponse};
+
+use std::path::PathBuf;
+
+use console::Style;
 use mongodb::bson::{doc};
 use crate::db_connection::DbConnection;
 
 mod db_connection;
 
-#[tokio::main]
-async fn main() {
+async fn single_page_app() -> Result<fs::NamedFile> {
+    let path: PathBuf = PathBuf::from("../frontend/build/index.html");
+    Ok(fs::NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+pub async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    let blue = Style::new()
+        .blue();
+
+    // 2.
+    let prefix = "127.0.0.1:"; // // Use 0.0.0.0 instead of localhost or 127.0.0.1 to use Actix with docker
+    let port = 8080; // We will use 80 for aws with env variable.
+    let target = format!("{}{}", prefix, port);
+
     let mut db_conn: DbConnection = DbConnection::new("mongodb://localhost:27017".to_string()).await;
     db_conn.set_database("ajseDB");
     db_conn.set_collection("estimates");
-    db_conn.insert_document(doc! {"name" : "Brady Smith", "address" : "8903124 main street."}, None).await;
-    db_conn.print_databases().await;
+
+    println!("\nServer ready at {}", blue.apply_to(format!("http://{}",&target)));
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .route("/", web::get().to(single_page_app))
+            .route("/user", web::get().to(single_page_app))
+            .service(fs::Files::new("/", "../frontend/build").index_file("index.html"))
+    })
+        .bind(&target) // Separate prefix, port, target, println! not to show "Not registered service error"
+        .unwrap()
+        .run()
+        .await
 }
+
