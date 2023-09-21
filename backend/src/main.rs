d1d644ac7a13b783a7053f46240c9d971a7c9d41
Brady Smith <bsmith8@catamount.wcu.edu>
2023-09-21 19:31:52 -0400

    Overhauled backend to become an api proxy server. Now when there are calls to the server it will create, find, delete, edit, and retrieve all users from the mongodb database. Needs to be connected to the frontend. But has been tested with postman program. (6 hours)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,52 +1,51 @@
 extern crate actix_web;
-extern crate actix_files;
 extern crate console;
 
-use actix_files as fs;
-use actix_web::{App, HttpServer, web, middleware, Result, Responder, HttpResponse};
+//add the modules
+mod api;
+mod repository;
+mod model;
 
-use std::path::PathBuf;
-
+use actix_web::{App, HttpServer, middleware::Logger,  Responder};
+use actix_web::web::Data;
 use console::Style;
-use mongodb::bson::{doc};
-use crate::db_connection::DbConnection;
+use api::user_api::{create_user, index, get_user, update_user, delete_user, get_all_users};
+use repository::mongodb_repo::MongoRepoUser;
 
-mod db_connection;
-
-async fn single_page_app() -> Result<fs::NamedFile> {
-    let path: PathBuf = PathBuf::from("../frontend/build/index.html");
-    Ok(fs::NamedFile::open(path)?)
-}
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
+    std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
     let blue = Style::new()
         .blue();
 
-    // 2.
     let prefix = "127.0.0.1:"; // // Use 0.0.0.0 instead of localhost or 127.0.0.1 to use Actix with docker
     let port = 8080; // We will use 80 for aws with env variable.
     let target = format!("{}{}", prefix, port);
 
-    let mut db_conn: DbConnection = DbConnection::new("mongodb://localhost:27017".to_string()).await;
-    db_conn.set_database("ajseDB");
-    db_conn.set_collection("estimates");
+    let db = MongoRepoUser::init().await;
+    let db_data = Data::new(db);
 
     println!("\nServer ready at {}", blue.apply_to(format!("http://{}",&target)));
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
+        let logger = Logger::default();
         App::new()
-            .wrap(middleware::Logger::default())
-            .route("/", web::get().to(single_page_app))
-            .route("/user", web::get().to(single_page_app))
-            .service(fs::Files::new("/", "../frontend/build").index_file("index.html"))
+            .wrap(logger)
+            .app_data(db_data.clone())
+            .service(create_user)
+            .service(get_user)
+            .service(update_user)
+            .service(delete_user)
+            .service(get_all_users)
+            .service(index)
     })
-        .bind(&target) // Separate prefix, port, target, println! not to show "Not registered service error"
-        .unwrap()
+        .bind(&target)?
         .run()
         .await
 }
+
 
---
+++ backend/src/repository/mongodb_repo.rs
@@ -0,0 +1,129 @@
+use std::env;
+use std::process::exit;
+use dotenv::dotenv;
+use mongodb::{
+    bson::{extjson::de::Error, doc, oid::ObjectId},
+    results::{ InsertOneResult},
+    options::ClientOptions,
+    Client, Collection,
+};
+use futures::stream::TryStreamExt; //add this
+use mongodb::results::{DeleteResult, UpdateResult};
+use crate::model::user_model::User;
+
+pub struct MongoRepoUser {
+    col: Collection<User>,
+}
+
+impl MongoRepoUser {
+    pub async fn init() -> Self {
+        dotenv().ok();
+        let database_address = match env::var("MONGOURL") {
+            Ok(v) => v.to_string(),
+            Err(_) => format!("Error loading env variable"),
+        };
+        let client_options = ClientOptions::parse(database_address).await;
+        let mut client_options = match client_options {
+            Ok(client_options) => client_options,
+            Err(_) => {
+                println!("Error: Could not parse address. Check the address rust is trying to connect to.");
+                exit(1);
+            }
+        };
+
+        client_options.app_name = Some("Job Estimator and Scheduler".to_string());
+
+        let client = Client::with_options(client_options);
+        let client = match client {
+            Ok(client) => client,
+            Err(_) => {
+                println!("Error: Could not connect to the repository. Check if mongoDB is running.");
+                exit(1);
+            }
+        };
+        println!("Successfully connected to the database.");
+        let db = client.database("ajseDB");
+        let col: Collection<User> = db.collection("users");
+        MongoRepoUser { col }
+    }
+
+    pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
+        let new_doc = User {
+            id: None,
+            name: new_user.name,
+            location: new_user.location,
+            title: new_user.title,
+        };
+        let user = self
+            .col
+            .insert_one(new_doc, None)
+            .await
+            .ok()
+            .expect("Error creating user");
+        Ok(user)
+    }
+
+    pub async fn get_user(&self, id: &String) -> Result<User, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! {"_id": obj_id};
+        let user_detail = self
+            .col
+            .find_one(filter, None)
+            .await
+            .ok()
+            .expect("Error getting user's detail");
+        Ok(user_detail.unwrap())
+    }
+
+    pub async fn update_user(&self, id: &String, new_user: User) -> Result<UpdateResult, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! {"_id": obj_id};
+        let new_doc = doc! {
+                "$set":
+                    {
+                        "id": new_user.id,
+                        "name": new_user.name,
+                        "location": new_user.location,
+                        "title": new_user.title
+                    },
+            };
+        let updated_doc = self
+            .col
+            .update_one(filter, new_doc, None)
+            .await
+            .ok()
+            .expect("Error updating user");
+        Ok(updated_doc)
+    }
+
+    pub async fn delete_user(&self, id: &String) -> Result<DeleteResult, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! {"_id": obj_id};
+        let user_detail = self
+            .col
+            .delete_one(filter, None)
+            .await
+            .ok()
+            .expect("Error deleting user");
+        Ok(user_detail)
+    }
+
+    pub async fn get_all_users(&self) -> Result<Vec<User>, Error> {
+        let mut cursors = self
+            .col
+            .find(None, None)
+            .await
+            .ok()
+            .expect("Error getting list of users");
+        let mut users: Vec<User> = Vec::new();
+        while let Some(user) = cursors
+            .try_next()
+            .await
+            .ok()
+            .expect("Error mapping through cursor")
+        {
+            users.push(user)
+        }
+        Ok(users)
+    }
+}
