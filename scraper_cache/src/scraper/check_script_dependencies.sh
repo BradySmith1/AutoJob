2c38e0a1de57985025eefac527aab7a9c00e8472
Brady Smith <bsmith8@catamount.wcu.edu>
2023-12-01 11:19:32 -0500

    (small implementation) implemented a check for having google chrome installed. (30 minutes)

--- scraper_cache/src/scraper/check_script_dependencies.sh
+++ scraper_cache/src/scraper/check_script_dependencies.sh
@@ -12,5 +12,14 @@ if ! pip show selenium &>/dev/null; then
     echo "selenium is not installed. Installing now..."
     pip install selenium
 else
-    echo "selenium is installed. Starting Server."
+    echo "selenium is installed. Proceeding..."
+fi
+
+if ! google-chrome --version &>/dev/null; then
+    echo "Google Chrome is not installed. Installing now..."
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt -f install ./google-chrome-stable_current_amd64.deb
+    rm google-chrome-stable_current_amd64.deb
+else
+    echo "Google Chrome is installed. Starting Server."
 fi
