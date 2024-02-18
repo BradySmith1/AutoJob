f83fffb4b5c60d8678114461328bf253df3297bc
Andrew Monroe <armonroe1@catamount.wcu.edu>
2024-02-17 21:38:07 -0500

    Added a log out page

--- estimator/src/components/accountPage/Account.js
+++ estimator/src/components/accountPage/Account.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useContext } from "react"
+import { AuthContext } from "../authentication/AuthContextProvider"
+import "./Account.css"
 
 function Account(){
+    const {jwt, setJwt} = useContext(AuthContext);
+
     return(
-        <div>
+        <div className="AccountWrapper">
+            <button className="button" onClick={() => {
+                setJwt(0);
+                document.cookie = "AutoJobRefresh=;expires=" + new Date(0).toUTCString();
+                window.location.reload(true);
+            }}>
+                Log Out
+            </button>
             
         </div>
     );
