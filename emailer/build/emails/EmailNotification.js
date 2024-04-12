4ddf49363f137b3fd55b5d02c0378aa67421a1dd
YourPalDrewdle <andrewmonroe289@gmail.com>
2024-04-12 14:55:44 -0400

    Email notification is now nicely styled

--- emailer/build/emails/EmailNotification.js
+++ emailer/build/emails/EmailNotification.js
@@ -1,6 +1,7 @@
-import { Html, Heading } from "@react-email/components";
+import { Body, Container, Head, Hr, Html, Text } from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
 import * as React from "react";
-import { jsxs as _jsxs, jsx as _jsx } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 var grandTotal = 0;
 export default function EmailNotification(props) {
   var total = 0;
@@ -12,15 +13,40 @@ export default function EmailNotification(props) {
   });
   grandTotal = total.toFixed(2);
   return /*#__PURE__*/_jsxs(Html, {
-    children: [/*#__PURE__*/_jsxs(Heading, {
-      as: "h1",
-      children: ["Hello, ", props.estimateData.user.fName, ", Your Estimate Is Ready."]
-    }), /*#__PURE__*/_jsxs(Heading, {
-      as: "h2",
-      children: ["Your estimate total is: $", grandTotal, "."]
-    }), /*#__PURE__*/_jsx(Heading, {
-      as: "h2",
-      children: "View the attached PDF to see the itemized estimate."
+    children: [/*#__PURE__*/_jsx(Head, {}), /*#__PURE__*/_jsx(Tailwind, {
+      children: /*#__PURE__*/_jsx(Body, {
+        className: "bg-white my-auto mx-auto font-sans px-2",
+        children: /*#__PURE__*/_jsxs(Container, {
+          className: "border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]",
+          children: [/*#__PURE__*/_jsx(Text, {
+            className: "text-black text-[32px] leading-[24px] text-center",
+            children: /*#__PURE__*/_jsx("strong", {
+              children: "Your Estimate is Ready"
+            })
+          }), /*#__PURE__*/_jsxs(Text, {
+            className: "text-black text-[14px] leading-[24px]",
+            children: ["Hello ", /*#__PURE__*/_jsxs("strong", {
+              children: [props.estimateData.user.fName, " ", props.estimateData.user.lName]
+            }), ", Your estimate has been completed."]
+          }), /*#__PURE__*/_jsxs(Text, {
+            className: "text-black text-[14px] leading-[24px]",
+            children: ["Your estimate total is: ", /*#__PURE__*/_jsx("strong", {
+              children: grandTotal
+            })]
+          }), /*#__PURE__*/_jsx(Text, {
+            className: "text-black text-[14px] leading-[24px]",
+            children: "To view the itemized bill, please see the attached PDF file. Please contact the company to notify them on wether you will accept or deny this estimate."
+          }), /*#__PURE__*/_jsx(Hr, {
+            className: "border border-solid border-[#eaeaea] my-[26px] mx-0 w-full"
+          }), /*#__PURE__*/_jsxs(Text, {
+            className: "text-[#666666] text-[12px] leading-[24px]",
+            children: ["This estimate was intended for", " ", /*#__PURE__*/_jsxs("span", {
+              className: "text-black",
+              children: [props.estimateData.user.fName, " ", props.estimateData.user.lName]
+            }), ". This email is an automated message from AutoJob. If you were not expecting this message, you can ignore this email."]
+          })]
+        })
+      })
     })]
   });
 }
--- emailer/src/emails/EmailNotification.jsx
+++ emailer/src/emails/EmailNotification.jsx
@@ -1,4 +1,12 @@
-import { Html, Heading } from "@react-email/components";
+import {
+  Body,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Text,
+} from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
 import * as React from "react";
 
 var grandTotal = 0;
@@ -17,16 +25,34 @@ export default function EmailNotification(props) {
 
   return (
     <Html>
-      <Heading as='h1'>
-        Hello, {props.estimateData.user.fName}, Your Estimate Is Ready.
-      </Heading>
-      <Heading as='h2'>
-        Your estimate total is: ${grandTotal}.
-      </Heading>
-      <Heading as='h2'>
-        View the attached PDF to see the itemized estimate.
-      </Heading>
-
+      <Head />
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans px-2">
+          <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
+            <Text className="text-black text-[32px] leading-[24px] text-center">
+              <strong>Your Estimate is Ready</strong>
+            </Text>
+            <Text className="text-black text-[14px] leading-[24px]">
+              Hello <strong>{props.estimateData.user.fName} {props.estimateData.user.lName}</strong>, Your estimate has been completed.
+            </Text>
+            <Text className="text-black text-[14px] leading-[24px]">
+              Your estimate total is: <strong>{grandTotal}</strong>
+            </Text>
+            <Text className="text-black text-[14px] leading-[24px]">
+              To view the itemized bill, please see the attached PDF file.
+              Please contact the company to notify them on wether you will accept
+              or deny this estimate.
+            </Text>
+            <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+            <Text className="text-[#666666] text-[12px] leading-[24px]">
+              This estimate was intended for{" "}
+              <span className="text-black">{props.estimateData.user.fName} {props.estimateData.user.lName}</span>. This email is an automated
+              message from AutoJob. If you
+              were not expecting this message, you can ignore this email.
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 }
