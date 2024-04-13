004b18b41f3450e9e28fe20c5df09ccaca5fe467
YourPalDrewdle <andrewmonroe289@gmail.com>
2024-04-13 16:02:10 -0400

    changed emailer to use mongodb (unteste)

--- emailer/build/index.js
+++ emailer/build/index.js
@@ -4,8 +4,24 @@ import ReactPDF from '@react-pdf/renderer';
 import BillPDF from './pdfs/BillPDF.js';
 import 'dotenv/config';
 import * as fs from 'fs';
+import { MongoClient } from 'mongodb';
 import { jsx as _jsx } from "react/jsx-runtime";
+const uri = "mongodb://localhost:27017";
 const apiKey = "re_JuU7fjp3_N7PywM8GkyoXWe4KVqjRKhex";
+const findDocuments = function (db, id) {
+  const collection = db.collection('jobEstimates');
+  collection.find({
+    "_id": id
+  }).toArray(function (err, documents) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Documents retrieved successfully");
+    console.log(documents);
+    return documents;
+  });
+};
 
 //Taken from Akhil Anand on Medium https://medium.com/@akhilanand.ak01/converting-streams-to-buffers-a-practical-guide-745fc2f77728
 async function streamToBuffer(readableStream) {
@@ -74,12 +90,27 @@ function readJsonFile(path) {
   }
   return JSON.parse(data);
 }
+async function getEstimateData(id, uID) {
+  var estimateData = {};
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    const db = client.db(`${uID}`);
+    estimateData = findDocuments(db, id)[0];
+  } catch (e) {
+    console.error("MongoDB Error");
+    process.exit(3);
+  } finally {
+    await client.close();
+  }
+  return estimateData;
+}
 async function main() {
-  if (process.argv.length !== 3) {
+  if (process.argv.length !== 4) {
     console.error("ERROR: Invalid Command Line Args");
     process.exit(1);
   }
-  const estimateData = readJsonFile(process.argv[2]);
+  const estimateData = getEstimateData(process.argv[2], process.argv[3]);
   sendEmail(estimateData);
 }
 main();
