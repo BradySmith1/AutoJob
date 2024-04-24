99ddc727d946ad15024b2723a5d0d9fe483e6cd7
Brady Smith <bksmith8@catamount.wcu.edu>
2024-04-24 12:07:45 -0400

    Deleted un needed scraper model and added a Readme to the web scraper.

--- resource_server/src/model/mod.rs
+++ resource_server/src/model/mod.rs
@@ -4,5 +4,4 @@ pub mod image_model;
 pub mod jwt_model;
 pub mod model_trait;
 pub mod schema_model;
-pub mod scraper_model;
 pub mod user_model;
---
+++ web_scraper_python/README.md
@@ -0,0 +1,7 @@
+Current version that this program is running on is python 3.12
+
+Imports needed
+* Flask
+* pymongo
+* selenium
+* Google Chrome
