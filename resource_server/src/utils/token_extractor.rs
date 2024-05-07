bb79c5d7ffcb9a5c1abbea3aaaa1b1a11554fdb6
Brady Smith <bksmith8@catamount.wcu.edu>
2024-05-07 11:44:08 -0400

    More Documentation.

--- resource_server/src/utils/token_extractor.rs
+++ resource_server/src/utils/token_extractor.rs
@@ -13,6 +13,7 @@ use serde::{Deserialize, Serialize};
 use std::future::Future;
 use std::pin::Pin;
 
+/// The claims struct is used to store the JWT claims.
 #[derive(Serialize, Deserialize)]
 struct Claims {
     userid: String,
@@ -20,6 +21,7 @@ struct Claims {
     exp: usize,
 }
 
+/// The AuthenticationToken struct is used to store the JWT token.
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AuthenticationToken {
     pub userid: String,
@@ -27,9 +29,20 @@ pub struct AuthenticationToken {
     exp: usize,
 }
 
+/// The FromRequest trait is used to extract the JWT token from the request header.
 impl FromRequest for AuthenticationToken {
+    // The Error type is used to represent the error that might occur when extracting the token.
     type Error = ActixWebError;
+    // The Future type is used to represent the result of the extraction process.
     type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+
+    /// The from_request function is used to extract the JWT token from the request header.
+    /// It returns a Future object containing the result of the extraction process.
+    /// # Parameters
+    /// req: A reference to the HttpRequest object.
+    /// payload: A reference to the Payload object.
+    /// # Returns
+    /// A Future object containing the result of the extraction process.
     fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
         let req = req.clone();
         Box::pin(async move {
@@ -52,19 +65,23 @@ impl FromRequest for AuthenticationToken {
                 return Err(ErrorUnauthorized("Authentication token has foreign chars!"));
             }
 
+            // the secret for the JWT token
             let secret: &str = &req.app_data::<web::Data<String>>().unwrap();
 
+            // Decode the JWT token
             let token_result: Result<TokenData<Claims>, JwtError> = decode::<Claims>(
                 &authentication_token,
                 &DecodingKey::from_secret(secret.as_ref()),
                 &Validation::new(Algorithm::HS256),
             );
 
+            // Check if the token is valid
             if token_result.is_err() {
                 return Err(ErrorUnauthorized("Invalid authentication token sent!"));
             }
 
             let token = token_result.unwrap();
+            // Create an AuthenticationToken object
             let auth_token = AuthenticationToken {
                 userid: token.claims.userid.clone(),
                 issuerid: token.claims.issuerid,
@@ -79,14 +96,28 @@ impl FromRequest for AuthenticationToken {
     }
 }
 
+/// The check_auth_mongodb function is used to check the validity of the JWT token.
+///
+/// # Parameters
+///
+/// * `token` - An AuthenticationToken object containing the JWT token.
+///
+/// # Returns
+///
+/// A Result object containing the result of the operation. If the token is valid,
+/// it returns an Ok result with a message.
+/// If the token is invalid, it returns an Err result with an error message.
 async fn check_auth_mongodb(token: AuthenticationToken) -> Result<String, Error> {
+    // Initialize the MongoDB repository
     let db: MongoRepo<JWT> = MongoRepo::init("tokens", "admin").await;
     let doc = doc! {"userid" : &token.userid};
+    // Get the token from the database
     let result = db.get_documents_by_attribute(doc).await.unwrap();
     let stored_token = match result.get(0) {
         None => return Err(ErrorUnauthorized("No JWT Matches in DB")),
         Some(token) => token,
     };
+    // Check if the token is valid
     if stored_token.userid == token.userid
         && stored_token.issuerid == token.issuerid
         && stored_token.exp > (Utc::now().timestamp() as usize)
